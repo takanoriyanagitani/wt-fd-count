5bf09b20807fdb4bf3672faed782a6ff284f661d
yanagitani <takanori.yanagitani@gmail.com>
2026-06-09 07:56:52 +0900

    chore: wasi related shells

---
+++ opt.sh
@@ -0,0 +1,12 @@
+iwasi=./fcnt.wasm
+
+wasm-opt \
+	-Oz \
+	-o opt.wasm \
+	--enable-simd \
+	--enable-relaxed-simd \
+	--enable-bulk-memory \
+	--enable-nontrapping-float-to-int \
+	--enable-multivalue \
+	--enable-tail-call \
+	"${iwasi}"
